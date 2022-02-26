a9cd719ce37a890fa26cd499ac481b4cb65388a3
vnescape <vnescape@protonmail.com>
2022-02-26 15:44:33 +0100

    Allocate and write memory into process

--- Simple-DLL-Injector/src/main.cpp
+++ Simple-DLL-Injector/src/main.cpp
@@ -11,8 +11,24 @@ int main(void) {
 		list.PrintProcessName(list.aProcesses[i]);
 		std::cout << "\n";
 	}
-	list.GetUserProcessID();
-	list.GetUserDLLPath();
+	int procID = list.GetUserProcessID();
+	std::string dllPath = list.GetUserDLLPath();
+
+
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procID);
+
+	if (hProc == NULL)
+	{
+		std::cout << "Could not OpenProcess: " << GetLastError();
+	}
+
+	LPVOID baseAddress = VirtualAllocEx(hProc,
+										0,
+										MAX_PATH,
+										MEM_COMMIT | MEM_RESERVE,
+										PAGE_READWRITE);
+	WriteProcessMemory(hProc, baseAddress, dllPath.c_str(), dllPath.length(), 0);
+
 	std::cin.get();
 	return 0;
 }
