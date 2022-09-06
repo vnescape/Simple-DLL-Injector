ccd7ed0b53966bd531a03c000073ef2f4d7fe825
vnescape <vnescape@protonmail.com>
2022-09-06 11:12:55 +0200

    Add PrintModules() from MSDN as an example

--- Simple-DLL-Injector/src/main.cpp
+++ Simple-DLL-Injector/src/main.cpp
@@ -1,7 +1,64 @@
 #include "Process.h"
 #include <iostream>
 #include <tlhelp32.h>
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <psapi.h>
 
+// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
+// and compile with -DPSAPI_VERSION=1
+
+#define PSAPI_VERSION 1
+#pragma comment(lib, "psapi.lib")
+
+
+// https://docs.microsoft.com/en-us/windows/win32/psapi/enumerating-all-modules-for-a-process
+int PrintModules(DWORD processID)
+{
+	HMODULE hMods[1024];
+	HANDLE hProcess;
+	DWORD cbNeeded;
+	unsigned int i;
+
+	// Print the process identifier.
+
+	printf("\nProcess ID: %u\n", processID);
+
+	// Get a handle to the process.
+
+	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
+		PROCESS_VM_READ,
+		FALSE, processID);
+	if (NULL == hProcess)
+		return 1;
+
+	// Get a list of all the modules in this process.
+
+	if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded))
+	{
+		for (i = 0; i < (cbNeeded / sizeof(HMODULE)); i++)
+		{
+			TCHAR szModName[MAX_PATH];
+
+			// Get the full path to the module's file.
+
+			if (GetModuleFileNameEx(hProcess, hMods[i], szModName,
+				sizeof(szModName) / sizeof(TCHAR)))
+			{
+				// Print the module name and handle value.
+
+				_tprintf(TEXT("\t%s (0x%08X)\n"), szModName, hMods[i]);
+			}
+		}
+	}
+
+	// Release the handle to the process.
+
+	CloseHandle(hProcess);
+
+	return 0;
+}
 
 // Checks if dll is loaded in the target process
 int checkForModule(std::string& dllPath, int procId)
@@ -154,6 +211,6 @@ int main(int argc, char** argv) {
 	}
 
 	std::string dllPath = absolutDllPath;
-
+	PrintModules(procId);
 	return simpleDLLInjection(procId, dllPath);
 }
